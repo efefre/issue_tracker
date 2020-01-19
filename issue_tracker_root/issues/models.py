ec6129b5fb62c30fa5d53bb0da6db1757a0f1c17
Kasia <efefre@icloud.com>
2020-01-19 17:24:13 +0100

    Add model: Project

--- issue_tracker_root/issues/models.py
+++ issue_tracker_root/issues/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.TextField(verbose_name=_('Project name'), unique=True)
+    slug = models.SlugField(max_length=10)
 
-
+    def __str__(self):
+        return self.name
