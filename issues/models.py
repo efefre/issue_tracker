f46939d82518d6f585a4cbc55c1a48e7da296247
Kasia <efefre@icloud.com>
2020-01-19 16:24:08 +0100

    Reinstall project

--- issues/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext as _
-
-# Create your models here.
-class Project(models.Model):
-    name = models.TextField(verbose_name=_('Project name'), unique=True)
-    slug = models.SlugField(max_length=10)
