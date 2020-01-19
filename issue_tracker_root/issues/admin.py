9bcc556607003f405ed06756d355c96b2f639cd8
Kasia <efefre@icloud.com>
2020-01-19 19:02:02 +0100

    Register Comment in admin site

--- issue_tracker_root/issues/admin.py
+++ issue_tracker_root/issues/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Issue
+from .models import Project, Issue, Comment
 
 admin.site.register(Project)
 admin.site.register(Issue)
+admin.site.register(Comment)
