bc366f2d86abe9477ef75d927a41a8dd2bf692e5
Kasia <efefre@icloud.com>
2020-01-20 18:39:46 +0100

    Fix pep8 warnings

--- issue_tracker_root/issues/models.py
+++ issue_tracker_root/issues/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
 import pendulum
 
+
 # Create your models here.
 class Project(models.Model):
     name = models.TextField(verbose_name=_('Project name'), unique=True)
@@ -10,6 +11,7 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Issue(models.Model):
 
@@ -31,13 +33,16 @@ class Issue(models.Model):
     ]
 
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='issues', verbose_name=_('Project'))
-    issue_type = models.CharField(choices=ISSUE_TYPE_CHOICES, default='task', verbose_name=_('Issue type'), max_length=5)
+    issue_type = models.CharField(choices=ISSUE_TYPE_CHOICES, default='task', verbose_name=_('Issue type'),
+                                  max_length=5)
     issue_status = models.CharField(choices=ISSUE_STATUS_CHOICES, default='to do', verbose_name=_('Issue status'),
-                                  max_length=20)
+                                    max_length=20)
     priority = models.CharField(choices=PRIORITY_CHOICES, default='low', verbose_name=_('Issue priority'),
-                                  max_length=10)
-    assingee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues_assignee', verbose_name=_('Assignee'))
-    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues_reporter', verbose_name=_('Reporter'))
+                                max_length=10)
+    assingee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues_assignee',
+                                 verbose_name=_('Assignee'))
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues_reporter',
+                                 verbose_name=_('Reporter'))
     created = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
     updated = models.DateTimeField(auto_now=True, verbose_name=_("Updated"))
     summary = models.TextField(verbose_name=_('Summary'), max_length=250)
@@ -46,6 +51,7 @@ class Issue(models.Model):
 
     def __str__(self):
         return f'{self.summary} (project: {self.project})'
+
 
 class Comment(models.Model):
     comment_text = models.TextField(verbose_name=_('Comment'))
@@ -59,8 +65,8 @@ class Comment(models.Model):
 
 
 def get_attachment_path(instance, filename):
-    datetime=pendulum.now('Europe/Warsaw')
-    datetime = datetime.to_datetime_string().replace(':','_')
+    datetime = pendulum.now('Europe/Warsaw')
+    datetime = datetime.to_datetime_string().replace(':', '_')
     return 'uploads/{0}/{1}-{2}'.format(instance.issue.id, datetime, filename)
 
 
