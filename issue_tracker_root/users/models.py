9e86646df7e3aad0f2f75cfb47c08e336906bb7e
Kasia <efefre@icloud.com>
2020-01-26 20:45:15 +0100

    Add profile_img field in CustomUser

--- issue_tracker_root/users/models.py
+++ issue_tracker_root/users/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
+def get_attachment_path(instance, filename):
+    return 'uploads/profile/{0}/{1}'.format(instance.username, filename)
+
+
 class CustomUser(AbstractUser):
-    pass
+    profile_img = models.FileField(upload_to=get_attachment_path, verbose_name=_('Profile image'))
 
     def __str__(self):
         return self.username
