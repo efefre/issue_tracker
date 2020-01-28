a84fb0cb79c310dbf368c51b4081ad8864062f80
Kasia <efefre@icloud.com>
2020-01-28 15:10:46 +0100

    Add class: form-control to fields in signup.html (js)

--- issue_tracker_root/static/js/custom.js
+++ issue_tracker_root/static/js/custom.js
@@ -1,1 +1,5 @@
+// signup.html
 $('input#id_username').addClass('form-control')
+$('input#id_email').addClass('form-control')
+$('input#id_password1').addClass('form-control')
+$('input#id_password2').addClass('form-control')
