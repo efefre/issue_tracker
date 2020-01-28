ce952999d06e3d89465e059caaa4e922f7a5ccdb
Kasia <efefre@icloud.com>
2020-01-28 12:39:49 +0100

    Install allauth

--- issue_tracker_root/issue_tracker/settings.py
+++ issue_tracker_root/issue_tracker/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'issues',
-    'users'
+    'users',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
