87882e988dc86d8c8326bfb252b391e072523595
Kasia <efefre@icloud.com>
2020-02-02 11:42:16 +0100

    Add url for ProjectsListView

--- issue_tracker_root/issues/urls.py
+++ issue_tracker_root/issues/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DashboardView
+from .views import DashboardView, ProjectsListView
 
 app_name = 'issues'
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('projects/', ProjectsListView.as_view(), name='projects-list'),
 ]
