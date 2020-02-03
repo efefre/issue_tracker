c392caf2d00c9e6bc7bee929dd006d15f9b259a9
Kasia <efefre@icloud.com>
2020-02-03 17:49:40 +0100

    Create AddProjectForm

---
+++ issue_tracker_root/issues/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Project
+
+
+class AddProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ('name', 'slug', 'status')
+
+        widgets = {'name' : forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Enter project name'}),
+                   'slug' : forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Enter slug (only letters)'}),
+                   'status' : forms.Select(attrs={'class' : 'form-control'})
+                   }
