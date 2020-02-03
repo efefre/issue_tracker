051cb61f9c906bda34fa55e0b0f196b8ff456632
Kasia <efefre@icloud.com>
2020-02-03 18:03:35 +0100

    Add UpdateProjectForm

--- issue_tracker_root/issues/forms.py
+++ issue_tracker_root/issues/forms.py
@@ -8,7 +8,23 @@ class AddProjectForm(forms.ModelForm):
         model = Project
         fields = ('name', 'slug', 'status')
 
-        widgets = {'name' : forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Enter project name'}),
-                   'slug' : forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'Enter slug (only letters)'}),
-                   'status' : forms.Select(attrs={'class' : 'form-control'})
+        widgets = {'name': forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder': 'Enter project name'}),
+                   'slug': forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder': 'Enter slug (only letters)'}),
+                   'status': forms.Select(attrs={'class': 'form-control'})
+                   }
+
+
+class UpdateProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ('name', 'slug', 'status')
+
+        widgets = {'name': forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder': 'Enter project name'}),
+                   'slug': forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder': 'Enter slug (only letters)'}),
+                   'status': forms.Select(attrs={'class': 'form-control'})
                    }
