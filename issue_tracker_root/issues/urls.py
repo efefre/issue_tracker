679bea85c6f01433723496929db4675f7d9a846c
Kasia <efefre@icloud.com>
2020-02-03 18:13:58 +0100

    Add url for DeleteProjectView

--- issue_tracker_root/issues/urls.py
+++ issue_tracker_root/issues/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import DashboardView, ProjectsListView, AddProjectView, UpdateProjectView
+from .views import DashboardView, ProjectsListView, AddProjectView, UpdateProjectView, DeleteProjectView
 
 app_name = 'issues'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('projects/', ProjectsListView.as_view(), name='projects-list'),
     path('add-project', AddProjectView.as_view(), name="add-project"),
     path('update-project/<int:pk>', UpdateProjectView.as_view(), name='update-project'),
+    path('delete-project/<int:pk>', DeleteProjectView.as_view(), name='delete-project'),
 ]
