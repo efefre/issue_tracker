9ea59b9e06e638b45d1dd71c5e1732976c9e4ebd
Kasia <efefre@icloud.com>
2020-02-15 17:36:15 +0100

    Add simple_tag: projects_list

---
+++ issue_tracker_root/issues/templatetags/issues_extras.py
@@ -0,0 +1,8 @@
+from django import template
+from ..models import Project
+
+register = template.Library()
+
+@register.simple_tag
+def projects_list():
+    return Project.objects.all().order_by('created')
