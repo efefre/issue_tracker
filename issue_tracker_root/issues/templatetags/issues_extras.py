d35f8162ffee7e8a94fe1afe573cb2b80f724fdc
Kasia <efefre@icloud.com>
2020-02-16 16:25:19 +0100

    Register filters to find tasks and bugs in issues

--- issue_tracker_root/issues/templatetags/issues_extras.py
+++ issue_tracker_root/issues/templatetags/issues_extras.py
@@ -6,3 +6,21 @@ register = template.Library()
 @register.simple_tag
 def projects_list():
     return Project.objects.all().order_by('-created')
+
+
+@register.filter
+def tasks(list):
+    tasks_list = []
+    for issue in list:
+        if issue.type == 'task':
+            tasks_list.append(issue)
+    return tasks_list
+
+
+@register.filter
+def bugs(list):
+    tasks_list = []
+    for issue in list:
+        if issue.type == 'bug':
+            tasks_list.append(issue)
+    return tasks_list
