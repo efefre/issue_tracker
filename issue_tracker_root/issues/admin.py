42f79c8641b8afa2d16990ef7a14c31f6703730d
Kasia <efefre@icloud.com>
2020-02-16 17:42:11 +0100

    Add custom admin panels

--- issue_tracker_root/issues/admin.py
+++ issue_tracker_root/issues/admin.py
@@ -1,17 +1,55 @@
 from django.contrib import admin
 from .models import Project, Issue, Comment, Attachment
+
 # Register your models here.
 
 
-class IssueAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {
-            'fields': ('project', 'type', 'status', 'priority', 'assignee', 'reporter',
-                       'summary', 'description', 'environment')
-        }),
-    )
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "slug", "created")
+    search_fields = ("name",)
+    list_filter = ("name", "status")
+    ordering = ("created",)
 
-admin.site.register(Project)
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("summary", "project", "type", "priority", "assignee", "reporter")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "project",
+                    "type",
+                    "status",
+                    "priority",
+                    "assignee",
+                    "reporter",
+                    "summary",
+                    "description",
+                    "environment",
+                )
+            },
+        ),
+    )
+    search_fields = ("summary", "project")
+    list_filter = ("project", "type", "assignee", "reporter")
+    ordering = ("project", "created")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("issue", "author", "created")
+    search_fields = ("author", "issue")
+    list_filter = ("issue", "author")
+    ordering = ("created",)
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ("issue", "file", "added")
+    list_filter = ("issue",)
+    ordering = ("added",)
+
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Issue, IssueAdmin)
-admin.site.register(Comment)
-admin.site.register(Attachment)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
